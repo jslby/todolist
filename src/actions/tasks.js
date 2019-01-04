3093a3619e5c2214bf40e57a070d2b78fe50f0e4
Jslby <me@jslby.ru>
2019-01-04 06:01:42 +0300

    retab

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -2,29 +2,29 @@ import C from '../constants';
 import {v4} from 'uuid';
 
 export const addTask = text =>
-	({
-		type: C.ADD,
-		id: v4(),
-		text,
-		createdAt: new Date().getTime(),
-		complited: true,
-		important: false
-	})
+  ({
+    type: C.ADD,
+    id: v4(),
+    text,
+    createdAt: new Date().getTime(),
+    complited: true,
+    important: false
+  })
 
 export const removeTask = id =>
-	({
-		type: C.REMOVE,
-		id
-	})
+  ({
+    type: C.REMOVE,
+    id
+  })
 
 export const toggleComplited = id =>
-	({
-		type: C.TOGGLE_COMPLITED,
-		id
-	})
+  ({
+    type: C.TOGGLE_COMPLITED,
+    id
+  })
 
 export const toggleImportant = (id) =>
-	({
-		type: C.TOGGLE_IMPORTANT,
-		id,
-	})
+  ({
+    type: C.TOGGLE_IMPORTANT,
+    id,
+  })
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,31 +1,31 @@
 import C from '../constants';
 
 export const tasks = (state = [], action) => {
-	switch(action.type){
-		case C.ADD:
-			return [
-				{
-					id: action.id,
-					text: action.text,
-					createdAt: action.createdAt,
-					complited: action.complited,
-					important: action.important
-				},
-				...state
-			]
-		case C.TOGGLE_COMPLITED:
-			return state.map(task => 
-				task.id === action.id ?
-					{...task, complited: !task.complited} : task
-			)
-		case C.TOGGLE_IMPORTANT:
-			return state.map(task =>
-				task.id === action.id ?
-				{...task, important: !task.important} : task
-			)
-		case C.REMOVE:
-			return state.filter(task => task.id !== action.id)
-		default:
-			return state
-	}
+  switch(action.type){
+    case C.ADD:
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          createdAt: action.createdAt,
+          complited: action.complited,
+          important: action.important
+        },
+        ...state
+      ]
+    case C.TOGGLE_COMPLITED:
+      return state.map(task => 
+        task.id === action.id ?
+          {...task, complited: !task.complited} : task
+      )
+    case C.TOGGLE_IMPORTANT:
+      return state.map(task =>
+        task.id === action.id ?
+        {...task, important: !task.important} : task
+      )
+    case C.REMOVE:
+      return state.filter(task => task.id !== action.id)
+    default:
+      return state
+  }
 }
